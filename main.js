5846777a5137e05cf4abfb8e8d92001975695506
AtayevZarif <atayev1234567890@gmail.com>
2024-03-16 02:57:36 +0500

    Math

---
+++ main.js
@@ -0,0 +1,10 @@
+let random = Math.round(Math.random() * 100);
+let multiply = random * 2;
+let division = random / 2;
+let remainder = random % 3;
+
+alert("Sizga hozir random sonlar chiqishni boshlidi")
+alert("Random orqali chiqqan son:" + " " + random)
+alert("Biz random sonni 2 ga ko'paytirmoqchi va bo'lmoqchimiz")
+alert("Random orqali chiqqan son:" + " " + random + " " + "edi" + "\n" + "biz uni 2 ga ko'paytirib" + " " + multiply + " " + "sonini chiqardik,"
+ + "\n" + "va shu sonni 2 ga bo'lib" + " " + division + " " + "sonini chiqardik," + "\n" + "va shu sonni 3 ga bo'lgandagi qoldiq" + " " + remainder + " " + "sonini chiqardik")
